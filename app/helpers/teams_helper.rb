1841c6aa832a040a47a3f36f5746a5d700ed3385
Andres Menendez <andmenendez@gmail.com>
2019-07-31 00:26:49 -0400

    updated teams_helper :start_date TO :date_start

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -4,7 +4,7 @@ module TeamsHelper
       out = []
     else
       out = []
-      Match.all.order(:start_date).reverse_order.each do |match|
+      Match.all.order(:date_start).reverse_order.each do |match|
         if match.radiant_id == team_id
           out << match
         elsif match.dire_id == team_id
