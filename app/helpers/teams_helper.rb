52d1d5a99efbd3d0fa2b1ce7e7a8aebd60337607
Andres Menendez <andmenendez@gmail.com>
2019-07-31 00:19:44 -0400

    fixed teams index

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -1,13 +1,17 @@
 module TeamsHelper
   def findMatches(team_id)
-    out = []
-    Match.all.order(:start_date).reverse_order.each do |match|
-      if match.radiant_id == team_id
-        out << match
-      elsif match.dire_id == team_id
-        out << match
+    if Match.all == []
+      out = []
+    else
+      out = []
+      Match.all.order(:start_date).reverse_order.each do |match|
+        if match.radiant_id == team_id
+          out << match
+        elsif match.dire_id == team_id
+          out << match
+        end
       end
+      out
     end
-    out
   end
 end
